7fc21a003abd4065733f5034ba85eba6253886c8
Valeria <valeriamarilef21@gmail.com>
2024-10-20 02:49:00 -0300

    Se agrega el proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { MascotaComponent } from './mascota/mascota.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent, FooterComponent, MascotaComponent],//Inyección de dependencia para que el componente sepa que 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
